d4a88cc707447f05c5a863d4a32d99f68ce2bcf5
Cedrick Paul C. Pangan <cedrickpangan29@gmail.com>
2021-09-02 12:00:29 +0800

    Adds Task section

--- App.js
+++ App.js
@@ -12,6 +12,19 @@ export default function App() {
             <Task text={'Task 1'}></Task>
         </View>
       </View>
+
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
